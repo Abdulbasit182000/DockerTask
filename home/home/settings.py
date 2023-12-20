5586237f2045af8c90c3a331c5c6767ed4285676
AbdulBasit182000 <abdulbasit.jalil@gmail.com>
2023-12-20 17:00:45 +0500

    added secret key to .env file

--- home/home/settings.py
+++ home/home/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u%gaosoai0s&+e0=d)ld#5&0+!z)iwj8e4p$!2hk%p$zz9ox-l'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
